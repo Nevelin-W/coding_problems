bc8c127172de4edba16ce9b941e34b7d9e8c5fdd
RobertsKSmits <90193490+Nevelin-W@users.noreply.github.com>
2024-08-06 13:43:57 +0000

    383 java

--- 383.RansomNote/JAVA/Ransom.java
+++ 383.RansomNote/JAVA/Ransom.java
@@ -1,19 +1,24 @@
 public class Ransom {
     public static void main(String[] args) {
-        String ransomNote = "aa", magazine = "bagreberba";
+        String ransomNote = "aad", magazine = "bagreberbsdvsdvsda";
+        
+        if (ransomNote.length() > magazine.length()) {
+            System.out.println("False");
+            return;
+        }
+        
         int[] alphabets_counter = new int[26];
 
         for (char c : magazine.toCharArray())
             alphabets_counter[c-'a']++;
 
         for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) System.out.println("False");
+            if (alphabets_counter[c-'a'] == 0) {
+                System.out.println("False");
+                return;
+            }
             alphabets_counter[c-'a']--;
         }
         System.out.println("True");
-        
-        
     }
-
-    
 }
